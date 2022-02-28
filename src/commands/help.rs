2cd52c16c631833cf24ec689222b133ddbf427d5
yakiimoninja <yakiimoninja@gmail.com>
2022-02-28 02:29:57 +0000

    better formatted the help message

--- src/commands/help.rs
+++ src/commands/help.rs
@@ -8,26 +8,27 @@ async fn help (ctx: &Context, msg: &Message) -> CommandResult {
 
     let help_message = r#"
 __**Commands**__
+
 • Command: `b.f` or `b.frames`. 
-• Displays the frame data of a move.
-• Use `b.f` or `b.frames` followed by the `character` and the `move` you want.
-• Example: `b.f baiken 236K` or `b.frames baiken 236K`.
+      Displays the frame data of a move.
+      Use `b.f` or `b.frames` followed by the `character` and the `move` you want.
+      Example: `b.f baiken 236K` or `b.frames baiken 236K`.
 
 • Command: `b.m` or `b.moves`.
-• Displays all the moves and their inputs of a character.
-• Use `b.m` or `b.moves` followed by the `character` you want.
-• Example: `b.m sol` or `b.moves sol`.
+      Displays all the moves and their inputs of a character.
+      Use `b.m` or `b.moves` followed by the `character` you want.
+      Example: `b.m sol` or `b.moves sol`.
 
 • Command: `b.a` or `b.aliases`.
-• Displays all the aliases for each special/super move of a character.
-• Use `b.a` or `b.aliases` followed by the `character` you want.
-• Example: `b.a leo` or `b.moves leo`.
+      Displays all the aliases for each special/super move of a character.
+      Use `b.a` or `b.aliases` followed by the `character` you want.
+      Example: `b.a leo` or `b.moves leo`.
 
 • Command: `b.u` or `b.update`.
-• Updates the frame data according to <https://dustloop.com/>.
+      Updates the frame data according to <https://dustloop.com>.
 
 • Command: `b.h` or `b.help` or `b.?`.
-• Displays this help message.
+      Displays this help message.
 
 __**Github**__
 <https://github.com/yakiimoninja/baiken-bot>
