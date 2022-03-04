4d86dcb4d0470820bb004204bd8df809d8688163
yakiimoninja <yakiimoninja@gmail.com>
2022-03-04 23:34:00 +0000

    refactored cause character image txts no longer exist

--- src/commands/update/char_json/data_to_json.rs
+++ src/commands/update/char_json/data_to_json.rs
@@ -1,17 +1,10 @@
-use std::{fs::{File, self}, io::Write};
+use std::{fs::File, io::Write};
 use crate::{Frames, CHARS};
 
 
 pub fn write_data_to_json(move_information: String, mut file: &File, char_count: usize){
 
     let move_information: Vec<&str> = move_information.split("|").collect();
-    let mut image_link_index = 0;
-
-    let mut character_image_links = fs::read_to_string(&("data/images/".to_owned() + CHARS.0[char_count] + ".txt"))
-                .expect(&("\nFailed to read 'data/images/".to_owned() + CHARS.0[char_count] + "'.txt' file."));
-
-    character_image_links = character_image_links.replace("\r", "");
-    let character_image_links: Vec<&str> = character_image_links.split("\n").collect();
 
     // Iterating through the vector containg the moves
     // With a step of 14 because most of the moves have 14 properties
@@ -45,7 +38,6 @@ pub fn write_data_to_json(move_information: String, mut file: &File, char_count:
                 active: move_information[x-9].to_string(),
                 recovery: move_information[x-8].to_string(),
                 counter: move_information[x-2].to_string(),
-                img_link: character_image_links[image_link_index].to_string(),
             }).unwrap();
 
             file.write_all(&vectored_frame_data)
@@ -56,8 +48,6 @@ pub fn write_data_to_json(move_information: String, mut file: &File, char_count:
                     
                     file.write(b",\n\t")
                         .expect(&("\nFailed to write ',\\n\\t' while serializing '".to_owned() + CHARS.0[char_count]+ ".json'."));
-                
-                    image_link_index += 1;
                 }
             }            
         }
@@ -83,7 +73,6 @@ pub fn write_data_to_json(move_information: String, mut file: &File, char_count:
                 active: move_information[x-8].to_string(),
                 recovery: move_information[x-7].to_string(),
                 counter: move_information[x-1].to_string(),
-                img_link: character_image_links[image_link_index].to_string(),
             }).unwrap();
             
             file.write_all(&vectored_frame_data)
@@ -95,7 +84,6 @@ pub fn write_data_to_json(move_information: String, mut file: &File, char_count:
                     file.write(b",\n\t")
                         .expect(&("\nFailed to write ',\\n\\t' while serializing '".to_owned() + CHARS.0[char_count]+ ".json'."));
                 
-                    image_link_index += 1;
                 }
             }  
 
