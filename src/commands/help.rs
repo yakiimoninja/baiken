04339102359e85210d9d2a75f1d43216ed93ba5a
yakiimoninja <yakiimoninja@gmail.com>
2022-05-01 19:03:16 +0000

    updated help message

--- src/commands/help.rs
+++ src/commands/help.rs
@@ -7,57 +7,16 @@ use serenity::prelude::*;
 async fn help(ctx: &Context, msg: &Message) -> CommandResult {
 
     let help_message = r#"
-__**Commands**__
-
-• Command: `b.f` or `b.frames`. 
-      Displays the frame data of a move.
-      Use `b.f` or `b.frames` followed by the `character` and the `move` you want.
-      Example: `b.f baiken 236K` or `b.frames baiken 236K`.
-
-• Command: `b.h` or `b.hitbox`. 
-      Displays the hitbox images of a move.
-      Use `b.h` or `b.hitbox` followed by the `character` and the `move` you want.
-      Example: `b.h goldlewis 4126H` or `b.hitbox goldlewis 4126H`.
-
-• Command: `b.m` or `b.moves`.
-      Displays all the moves and their inputs of a character.
-      Use `b.m` or `b.moves` followed by the `character` you want.
-      Example: `b.m sol` or `b.moves sol`.
-
-• Command: `b.a` or `b.aliases`.
-      Displays all the aliases for each special/super move of a character.
-      Use `b.a` or `b.aliases` followed by the `character` you want.
-      Example: `b.a leo` or `b.moves leo`.
-
-• Command: `b.u` or `b.update`.
-      Updates the frame data according to <https://dustloop.com>.
-
-• Command: `b.?` or `b.help`.
-      Displays this help message.
-
-__**Notes**__
-
-• Searching is case insensitive.
-      This works: `b.hitbox Ky DP`.
-      This also works: `b.h ky dp`.
-
-• You don't have to write the full name of a character.
-      This works: `b.frames giovanna 236K`.
-      This will also work: `b.f gio 236K`.
-
-• You can also use aliases when searching for moves.
-      This works: `b.frames Anji 236HP`.
-      This also works: `b.f Anji Needles`.
-
-• You can omit the '[]' when searching for a charged move.
-      This works: `b.frames May [4]6S`.
-      This also works: `b.f may 46S`.
-
-• When searching for charged dust attack use the alias `5D!`.
-      Example: `b.f chipp 5D!`.
-
-__**Author:**__  __<https://github.com/yakiimoninja/baiken>__
-__**Artwork:**__  __<https://twitter.com/gogalking>__
+__**Commands:**__
+__<https://github.com/yakiimoninja/baiken/blob/main/README.md#commands>__
+__**Usage Notes:**__
+__<https://github.com/yakiimoninja/baiken/blob/main/README.md#usage-notes>__
+__**Patch Notes:**__
+__<https://github.com/yakiimoninja/baiken/blob/main/patch_notes/v0.4.0.md>__
+__**Source Code:**__
+__<https://github.com/yakiimoninja/baiken>__
+__**Artwork:**__
+__<https://twitter.com/gogalking>__
 "#;
 
     msg.channel_id.say(&ctx.http, help_message).await?;
