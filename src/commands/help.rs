0678056d8a0d887983ebcf3f4845675f96b3f0d7
yakiimoninja <yakiimoninja@gmail.com>
2022-05-08 23:23:14 +0000

    changed formatting of help message

--- src/commands/help.rs
+++ src/commands/help.rs
@@ -9,12 +9,16 @@ async fn help(ctx: &Context, msg: &Message) -> CommandResult {
     let help_message = r#"
 __**Source Code:**__
 __<https://github.com/yakiimoninja/baiken>__
+
 __**Commands:**__
 __<https://github.com/yakiimoninja/baiken/blob/main/README.md#commands>__
+
 __**Usage Notes:**__
 __<https://github.com/yakiimoninja/baiken/blob/main/README.md#usage-notes>__
+
 __**Patch Notes:**__
-__<https://github.com/yakiimoninja/baiken/blob/main/patch_notes/v0.4.0.md>__
+__<https://github.com/yakiimoninja/baiken/blob/main/patch_notes/>__
+
 __**Artwork:**__
 __<https://twitter.com/gogalking/status/1307199393607553024>__
 "#;
