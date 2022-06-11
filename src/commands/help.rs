81dd53b7d002015d275be4b2ad0f54afba68059c
yakiimoninja <yakiimoninja@gmail.com>
2022-06-11 23:25:10 +0000

    updated help message

--- src/commands/help.rs
+++ src/commands/help.rs
@@ -7,8 +7,8 @@ use serenity::prelude::*;
 async fn help(ctx: &Context, msg: &Message) -> CommandResult {
 
     let help_message = r#"
-__**Source Code:**__
-__<https://github.com/yakiimoninja/baiken>__
+__**Patch Notes:**__
+__<https://github.com/yakiimoninja/baiken/releases>__
 
 __**Commands:**__
 __<https://github.com/yakiimoninja/baiken/blob/main/README.md#commands>__
@@ -16,8 +16,8 @@ __<https://github.com/yakiimoninja/baiken/blob/main/README.md#commands>__
 __**Usage Notes:**__
 __<https://github.com/yakiimoninja/baiken/blob/main/README.md#usage-notes>__
 
-__**Patch Notes:**__
-__<https://github.com/yakiimoninja/baiken/blob/main/patch-notes/>__
+__**Source Code:**__
+__<https://github.com/yakiimoninja/baiken>__
 
 __**Artwork:**__
 __<https://twitter.com/gogalking/status/1307199393607553024>__
