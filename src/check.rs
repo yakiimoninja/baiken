11ea0531a56fa8a2756fe3d549b4ee3b06211724
yakiimoninja <yakiimoninja@gmail.com>
2022-10-19 13:25:22 +0000

    removed the `init_json_exists` check

--- src/check.rs
+++ src/check.rs
@@ -1,4 +1,4 @@
-use crate::{CHARS, CharInfo, commands, Nicknames};
+use crate::{CHARS, Nicknames};
 use std::{fs, path::Path};
 
 // Collection of functions that check for stuff
@@ -24,40 +24,6 @@ pub fn data_folder_exists(init_check: bool) -> Option<String> {
             return Some(error_msg);
         }   
     }
-}
-
-pub fn init_json_exists(init_check: bool) -> Option<String> {
-
-    // If init.json doesnt exist initialize it
-    if Path::new("data/init.json").exists() == false {
-        commands::update::init_json::init_json();
-    }
-
-    // Reading init.json file
-    let data_from_file = fs::read_to_string("data/init.json")
-        .expect("\nFailed to read 'init.json' file.");
-
-    match serde_json::from_str::<Vec<CharInfo>>(&data_from_file) {
-        Ok(_) => {
-            println!("\nSuccesfully read 'init.json' file.");
-            return None;
-        },
-        Err(_) => {
-            let error_msg = "Failed to deserialize 'init.json' file. Delete 'init.json' from the 'data' folder and rerun the bot.".to_string();
-            
-            if init_check == true {
-                // Printing the error message in the console
-                // If it is the initial check
-                print!("\n");
-                panic!("{}", error_msg);
-            }
-            else {
-                // Returning the error message for in-discord printing
-                return Some(error_msg);
-            }
-            
-        },
-    };
 }
 
 pub fn nicknames_json_exists(init_check: bool) -> Option<String> {
