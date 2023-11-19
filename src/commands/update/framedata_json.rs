9c986077210f8f6988440ec63a8b0cffe85b44d1
yakiimoninja <yakiimoninja@gmail.com>
2023-11-19 15:46:30 +0000

    renamed `framedata` to `move_info`

--- src/commands/update/framedata_json.rs
+++ src/commands/update/framedata_json.rs
@@ -1,6 +1,6 @@
 use std::io::Write;
 use serde::Deserialize;
-use crate::{CHARS, Frames};
+use crate::{CHARS, MoveInfo};
 use std::fs::File;
 extern crate ureq;
 
@@ -42,98 +42,98 @@ pub async fn frames_to_json(mut char_page_response_json: String, mut file: &File
     // Ino low profile
     char_page_response_json = char_page_response_json.replace(r#" &lt;span class=&quot;tooltip&quot; &gt;Low Profile&lt;span class=&quot;tooltiptext&quot; style=&quot;&quot;&gt;When a character's hurtbox is entirely beneath an opponent's attack. This can be caused by crouching, certain moves, and being short.&lt;/span&gt;&lt;/span&gt;"#, "");
 
-    let mut framedata: Response = serde_json::from_str(&char_page_response_json).unwrap();
+    let mut move_info: Response = serde_json::from_str(&char_page_response_json).unwrap();
 
-    for x in 0..framedata.cargoquery.len() {
+    for x in 0..move_info.cargoquery.len() {
         
         // Replacing None values with a generic '-'
-        if framedata.cargoquery[x].title.input.is_none(){
-            framedata.cargoquery[x].title.input = Some("-".to_string());
+        if move_info.cargoquery[x].title.input.is_none(){
+            move_info.cargoquery[x].title.input = Some("-".to_string());
         }
         else{
             // Skips finish blow for sol
-            if *framedata.cargoquery[x].title.input.as_ref().unwrap() == "j.XX during Homing Jump" {
+            if *move_info.cargoquery[x].title.input.as_ref().unwrap() == "j.XX during Homing Jump" {
                 continue;
             }
         }
-        if framedata.cargoquery[x].title.name.is_none(){
-            framedata.cargoquery[x].title.name = Some(framedata.cargoquery[x].title.input.as_ref().unwrap().to_string());
+        if move_info.cargoquery[x].title.name.is_none(){
+            move_info.cargoquery[x].title.name = Some(move_info.cargoquery[x].title.input.as_ref().unwrap().to_string());
         }
         else {
             // Skips dash cancel entry, ino hoverdash chipp escape zato flight and finish blow 
-            if *framedata.cargoquery[x].title.name.as_ref().unwrap() == "Dash Cancel" || 
-            *framedata.cargoquery[x].title.name.as_ref().unwrap() == "Hoverdash" ||
-            *framedata.cargoquery[x].title.name.as_ref().unwrap() == "Finish Blow" ||
-            *framedata.cargoquery[x].title.name.as_ref().unwrap() == "Flight" ||
-            *framedata.cargoquery[x].title.name.as_ref().unwrap() == "Escape" {
+            if *move_info.cargoquery[x].title.name.as_ref().unwrap() == "Dash Cancel" || 
+            *move_info.cargoquery[x].title.name.as_ref().unwrap() == "Hoverdash" ||
+            *move_info.cargoquery[x].title.name.as_ref().unwrap() == "Finish Blow" ||
+            *move_info.cargoquery[x].title.name.as_ref().unwrap() == "Flight" ||
+            *move_info.cargoquery[x].title.name.as_ref().unwrap() == "Escape" {
                 continue;
             }
         }
-        if framedata.cargoquery[x].title.damage.is_none(){
-            framedata.cargoquery[x].title.damage = Some("-".to_string());
+        if move_info.cargoquery[x].title.damage.is_none(){
+            move_info.cargoquery[x].title.damage = Some("-".to_string());
         }
-        if framedata.cargoquery[x].title.guard.is_none(){
-            framedata.cargoquery[x].title.guard = Some("-".to_string());
+        if move_info.cargoquery[x].title.guard.is_none(){
+            move_info.cargoquery[x].title.guard = Some("-".to_string());
         }
-        if framedata.cargoquery[x].title.invuln.is_none(){
-            framedata.cargoquery[x].title.invuln = Some("-".to_string());
+        if move_info.cargoquery[x].title.invuln.is_none(){
+            move_info.cargoquery[x].title.invuln = Some("-".to_string());
         }
-        if framedata.cargoquery[x].title.startup.is_none(){
-            framedata.cargoquery[x].title.startup = Some("-".to_string());
+        if move_info.cargoquery[x].title.startup.is_none(){
+            move_info.cargoquery[x].title.startup = Some("-".to_string());
         }
-        if framedata.cargoquery[x].title.active.is_none(){
-            framedata.cargoquery[x].title.active = Some("-".to_string());
+        if move_info.cargoquery[x].title.active.is_none(){
+            move_info.cargoquery[x].title.active = Some("-".to_string());
         }
-        if framedata.cargoquery[x].title.recovery.is_none(){
-            framedata.cargoquery[x].title.recovery = Some("-".to_string());
+        if move_info.cargoquery[x].title.recovery.is_none(){
+            move_info.cargoquery[x].title.recovery = Some("-".to_string());
         }
-        if framedata.cargoquery[x].title.hit.is_none(){
-            framedata.cargoquery[x].title.hit = Some("-".to_string());
+        if move_info.cargoquery[x].title.hit.is_none(){
+            move_info.cargoquery[x].title.hit = Some("-".to_string());
         }
-        if framedata.cargoquery[x].title.block.is_none(){
-            framedata.cargoquery[x].title.block = Some("-".to_string());
+        if move_info.cargoquery[x].title.block.is_none(){
+            move_info.cargoquery[x].title.block = Some("-".to_string());
         }
-        if framedata.cargoquery[x].title.level.is_none(){
-            framedata.cargoquery[x].title.level = Some("-".to_string());
+        if move_info.cargoquery[x].title.level.is_none(){
+            move_info.cargoquery[x].title.level = Some("-".to_string());
         }
-        if framedata.cargoquery[x].title.riscgain.is_none(){
-            framedata.cargoquery[x].title.riscgain = Some("-".to_string());
+        if move_info.cargoquery[x].title.riscgain.is_none(){
+            move_info.cargoquery[x].title.riscgain = Some("-".to_string());
         }
-        if framedata.cargoquery[x].title.prorate.is_none(){
-            framedata.cargoquery[x].title.prorate = Some("-".to_string());
+        if move_info.cargoquery[x].title.prorate.is_none(){
+            move_info.cargoquery[x].title.prorate = Some("-".to_string());
         }
-        if framedata.cargoquery[x].title.counter.is_none(){
-            framedata.cargoquery[x].title.counter = Some("-".to_string());
+        if move_info.cargoquery[x].title.counter.is_none(){
+            move_info.cargoquery[x].title.counter = Some("-".to_string());
         }
 
         // Serializing frame data
-        let processed_framedata = serde_json::to_string(&Frames {
-            input: framedata.cargoquery[x].title.input.as_ref().unwrap().to_string(),
-            name: framedata.cargoquery[x].title.name.as_ref().unwrap().to_string(),
-            damage: framedata.cargoquery[x].title.damage.as_ref().unwrap().to_string(),
-            guard: framedata.cargoquery[x].title.guard.as_ref().unwrap().to_string(),
-            invincibility: framedata.cargoquery[x].title.invuln.as_ref().unwrap().to_string(),
-            startup: framedata.cargoquery[x].title.startup.as_ref().unwrap().to_string(),
-            active: framedata.cargoquery[x].title.active.as_ref().unwrap().to_string(),
-            recovery: framedata.cargoquery[x].title.recovery.as_ref().unwrap().to_string(),
-            hit: framedata.cargoquery[x].title.hit.as_ref().unwrap().to_string(),
-            block: framedata.cargoquery[x].title.block.as_ref().unwrap().to_string(),
-            level: framedata.cargoquery[x].title.level.as_ref().unwrap().to_string(),
-            riscgain: framedata.cargoquery[x].title.riscgain.as_ref().unwrap().to_string(),
-            scaling: framedata.cargoquery[x].title.prorate.as_ref().unwrap().to_string(),
-            counter: framedata.cargoquery[x].title.counter.as_ref().unwrap().to_string(),
+        let processed_move_info = serde_json::to_string(&MoveInfo {
+            input: move_info.cargoquery[x].title.input.as_ref().unwrap().to_string(),
+            name: move_info.cargoquery[x].title.name.as_ref().unwrap().to_string(),
+            damage: move_info.cargoquery[x].title.damage.as_ref().unwrap().to_string(),
+            guard: move_info.cargoquery[x].title.guard.as_ref().unwrap().to_string(),
+            invincibility: move_info.cargoquery[x].title.invuln.as_ref().unwrap().to_string(),
+            startup: move_info.cargoquery[x].title.startup.as_ref().unwrap().to_string(),
+            active: move_info.cargoquery[x].title.active.as_ref().unwrap().to_string(),
+            recovery: move_info.cargoquery[x].title.recovery.as_ref().unwrap().to_string(),
+            hit: move_info.cargoquery[x].title.hit.as_ref().unwrap().to_string(),
+            block: move_info.cargoquery[x].title.block.as_ref().unwrap().to_string(),
+            level: move_info.cargoquery[x].title.level.as_ref().unwrap().to_string(),
+            riscgain: move_info.cargoquery[x].title.riscgain.as_ref().unwrap().to_string(),
+            scaling: move_info.cargoquery[x].title.prorate.as_ref().unwrap().to_string(),
+            counter: move_info.cargoquery[x].title.counter.as_ref().unwrap().to_string(),
         }).unwrap();
         
-        write!(file, "{}", processed_framedata)
+        write!(file, "{}", processed_move_info)
         .expect(&("\nFailed to serialize '".to_owned() + CHARS[char_count]+ ".json'."));
         
         // Skip writting comma/tab if next and last iteration 
         // contains 'finish blow' in last the input field
-        if x == framedata.cargoquery.len() -2 &&
-        *framedata.cargoquery[x+1].title.input.as_ref().unwrap() == "j.XX during Homing Jump" {
+        if x == move_info.cargoquery.len() -2 &&
+        *move_info.cargoquery[x+1].title.input.as_ref().unwrap() == "j.XX during Homing Jump" {
             continue;
         }
-        else if x != framedata.cargoquery.len() - 1 {         
+        else if x != move_info.cargoquery.len() - 1 {         
             // Adding comma/tab
             // file.write(b",\n\t")
             //     .expect(&("\nFailed to write ',\\n\\t' while serializing '".to_owned() + CHARS[char_count]+ ".json'."));
