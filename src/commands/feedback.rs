a9a02c5b93a23284a25a0ea454f6c76a50e82df5
yakiimoninja <github@pns.slmail.me>
2024-10-29 23:35:34 +0000

    made command response ephemeral

--- src/commands/feedback.rs
+++ src/commands/feedback.rs
@@ -3,7 +3,7 @@ use colored::Colorize;
 use crate::{Context, Error};
 
 /// Sends feedback or requests to the dev.
-#[poise::command(prefix_command, slash_command)]
+#[poise::command(prefix_command, slash_command, ephemeral)]
 pub async fn feedback(
     ctx: Context<'_>,
     #[description = "Message for the dev."] text: String,
