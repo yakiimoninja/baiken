a58ec3e9b27f96d59cf18b3ea76d5be42ff0def3
yakiimoninja <github@pns.slmail.me>
2024-11-14 04:03:16 +0000

    change `/feedback` to impl modal

--- src/commands/feedback.rs
+++ src/commands/feedback.rs
@@ -1,28 +1,57 @@
-use std::{fs::OpenOptions, io::Write};
+use std::{fs::OpenOptions, io::Write, time::Duration};
 use colored::Colorize;
+use poise::execute_modal;
 use crate::{Context, Error};
 
-/// Send feedback or requests to the dev.
+#[derive(Debug, poise::Modal)]
+#[allow(dead_code)] // fields only used for Debug print
+#[name = "Feedback/Request Submission"]
+struct Submission{
+    #[min_length = 3 ]
+    #[max_length = 20 ]
+    #[placeholder = r#""Select "feature", "bug" or "other""#]
+    subject: String,
+    #[paragraph]
+    #[min_length = 10 ]
+    #[max_length = 200 ]
+    #[placeholder = "Explain in detail"]
+    text: String,
+}
+
+/// Send feedback or requests.
 #[poise::command(prefix_command, slash_command, ephemeral)]
 pub async fn feedback(
     ctx: Context<'_>,
-    #[description = "Message for the dev."] text: String,
 ) -> Result<(), Error> {
 
-    // Creating character json file
-    let mut file = OpenOptions::new()
-        .create(true)
-        .append(true)
-        .open("request.txt")
-        .expect("\nFailed to open 'request.txt' file.");
-    
-    let new_text = text.to_owned() + "\n\n";
+    let Context::Application(ctx) = ctx else {
+        unreachable!()
+    };
 
-    write!(file, "{}", new_text)
-        .expect("\nFailed to write to 'request.txt'");
-    
-    println!("{}", "Done writting to 'request.txt'".yellow());
-    ctx.say("Submitted successfully!").await?;
+    // Write to file only if modal isnt empty
+    if let Some(data) = execute_modal::<_,_, Submission>(ctx, None, Some(Duration::from_secs(60 * 2))).await? {
+        // Creating character json file
+        let mut file = OpenOptions::new()
+            .create(true)
+            .append(true)
+            .open("request.txt")
+            .expect("\nFailed to open 'request.txt' file.");
+        
+        let new_text = "".to_owned()
+            + "\n[Subject: " + &data.subject
+            + "]\n" + &data.text + "\n\n";
+
+        write!(file, "{}", new_text)
+            .expect("\nFailed to write to 'request.txt'");
+        
+        println!("{}", "Done writting to 'request.txt'".yellow());
+        ctx.say("Submitted successfully!").await?;
+    }
+    else {
+
+        println!("{}", "Feedback modal timed out.".red());
+        ctx.say("Form timed out.").await?;
+    }
 
     Ok(())
 }
