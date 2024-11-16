06c1b156a914140a9f3ab495555a366df1f40b65
yakiimoninja <github@pns.slmail.me>
2024-11-16 14:25:02 +0000

    change to parse more framedata fields

--- src/commands/update/framedata_json.rs
+++ src/commands/update/framedata_json.rs
@@ -17,24 +17,42 @@ struct Data {
 }
 
 #[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
 struct Title {
     input: Option<String>,
     name: Option<String>,
     damage: Option<String>,
     guard: Option<String>,
-    invuln: Option<String>,
     startup: Option<String>,
     active: Option<String>,
     recovery: Option<String>,
-    #[serde(rename = "onHit")]
-    hit: Option<String>,
+    on_hit: Option<String>,
     #[serde(rename = "onBlock")]
-    block: Option<String>,
+    on_block: Option<String>,
     level: Option<String>,
-    #[serde(rename = "riscGain")]
-    riscgain: Option<String>,
-    prorate: Option<String>,
     counter: Option<String>,
+    #[serde(rename = "type")]
+    move_type: Option<String>,
+    #[serde(rename = "riscGain")]
+    risc_gain: Option<String>,
+    #[serde(rename = "riscLoss")]
+    risc_loss: Option<String>,
+    #[serde(rename = "wallDamage")]
+    wall_damage: Option<String>,
+    input_tension: Option<String>,
+    chip_ratio: Option<String>,
+    #[serde(rename = "OTGRatio")]
+    otg_ratio: Option<String>,
+    #[serde(rename = "prorate")]
+    scaling: Option<String>,
+    #[serde(rename = "invuln")]
+    invincibility: Option<String>,
+    cancel: Option<String>,
+    caption: Option<String>,
+    notes: Option<String>,
+    hitbox_caption: Option<String>,
+    //images: Option<String>,
+    //hitboxes: Option<String>,
 }
 
 pub async fn frames_to_json(mut char_page_response_json: String, mut file: &File, char_count: usize) {
@@ -48,6 +66,7 @@ pub async fn frames_to_json(mut char_page_response_json: String, mut file: &File
 
     let mut moves_info: Response = serde_json::from_str(&char_page_response_json).unwrap();
     let mut vec_processed_moves_info = Vec::new();
+    println!("{:#?}", moves_info);
 
     for x in 0..moves_info.cargoquery.len() {
         
@@ -74,15 +93,18 @@ pub async fn frames_to_json(mut char_page_response_json: String, mut file: &File
                 continue;
             }
         }
+
+
+
+
         if moves_info.cargoquery[x].title.damage.is_none(){
             moves_info.cargoquery[x].title.damage = Some("-".to_string());
         }
         if moves_info.cargoquery[x].title.guard.is_none(){
             moves_info.cargoquery[x].title.guard = Some("-".to_string());
         }
-        if moves_info.cargoquery[x].title.invuln.is_none(){
-            moves_info.cargoquery[x].title.invuln = Some("-".to_string());
-        }
+
+
         if moves_info.cargoquery[x].title.startup.is_none(){
             moves_info.cargoquery[x].title.startup = Some("-".to_string());
         }
@@ -92,23 +114,59 @@ pub async fn frames_to_json(mut char_page_response_json: String, mut file: &File
         if moves_info.cargoquery[x].title.recovery.is_none(){
             moves_info.cargoquery[x].title.recovery = Some("-".to_string());
         }
-        if moves_info.cargoquery[x].title.hit.is_none(){
-            moves_info.cargoquery[x].title.hit = Some("-".to_string());
+        if moves_info.cargoquery[x].title.on_hit.is_none(){
+            moves_info.cargoquery[x].title.on_hit = Some("-".to_string());
         }
-        if moves_info.cargoquery[x].title.block.is_none(){
-            moves_info.cargoquery[x].title.block = Some("-".to_string());
+        if moves_info.cargoquery[x].title.on_block.is_none(){
+            moves_info.cargoquery[x].title.on_block = Some("-".to_string());
         }
         if moves_info.cargoquery[x].title.level.is_none(){
             moves_info.cargoquery[x].title.level = Some("-".to_string());
         }
-        if moves_info.cargoquery[x].title.riscgain.is_none(){
-            moves_info.cargoquery[x].title.riscgain = Some("-".to_string());
-        }
-        if moves_info.cargoquery[x].title.prorate.is_none(){
-            moves_info.cargoquery[x].title.prorate = Some("-".to_string());
-        }
         if moves_info.cargoquery[x].title.counter.is_none(){
             moves_info.cargoquery[x].title.counter = Some("-".to_string());
+        }
+        if moves_info.cargoquery[x].title.move_type.is_none(){
+            moves_info.cargoquery[x].title.move_type = Some("-".to_string());
+        }
+        if moves_info.cargoquery[x].title.risc_gain.is_none(){
+            moves_info.cargoquery[x].title.risc_gain = Some("-".to_string());
+        }
+        if moves_info.cargoquery[x].title.risc_loss.is_none(){
+            moves_info.cargoquery[x].title.risc_loss = Some("-".to_string());
+        }
+        if moves_info.cargoquery[x].title.scaling.is_none(){
+            moves_info.cargoquery[x].title.scaling = Some("-".to_string());
+        }
+        if moves_info.cargoquery[x].title.wall_damage.is_none(){
+            moves_info.cargoquery[x].title.wall_damage = Some("-".to_string());
+        }
+        if moves_info.cargoquery[x].title.input_tension.is_none(){
+            moves_info.cargoquery[x].title.input_tension = Some("-".to_string());
+        }
+        if moves_info.cargoquery[x].title.chip_ratio.is_none(){
+            moves_info.cargoquery[x].title.chip_ratio = Some("-".to_string());
+        }
+        if moves_info.cargoquery[x].title.otg_ratio.is_none(){
+            moves_info.cargoquery[x].title.otg_ratio = Some("-".to_string());
+        }
+        if moves_info.cargoquery[x].title.scaling.is_none(){
+            moves_info.cargoquery[x].title.scaling = Some("-".to_string());
+        }
+        if moves_info.cargoquery[x].title.invincibility.is_none(){
+            moves_info.cargoquery[x].title.invincibility = Some("-".to_string());
+        }
+        if moves_info.cargoquery[x].title.cancel.is_none(){
+            moves_info.cargoquery[x].title.cancel = Some("-".to_string());
+        }
+        if moves_info.cargoquery[x].title.caption.is_none(){
+            moves_info.cargoquery[x].title.caption = Some("-".to_string());
+        }
+        if moves_info.cargoquery[x].title.notes.is_none(){
+            moves_info.cargoquery[x].title.notes = Some("-".to_string());
+        }
+        if moves_info.cargoquery[x].title.hitbox_caption.is_none(){
+            moves_info.cargoquery[x].title.hitbox_caption = Some("-".to_string());
         }
 
         // Serializing frame data
@@ -117,16 +175,26 @@ pub async fn frames_to_json(mut char_page_response_json: String, mut file: &File
             name: moves_info.cargoquery[x].title.name.as_ref().unwrap().to_string(),
             damage: moves_info.cargoquery[x].title.damage.as_ref().unwrap().to_string(),
             guard: moves_info.cargoquery[x].title.guard.as_ref().unwrap().to_string(),
-            invincibility: moves_info.cargoquery[x].title.invuln.as_ref().unwrap().to_string(),
             startup: moves_info.cargoquery[x].title.startup.as_ref().unwrap().to_string(),
             active: moves_info.cargoquery[x].title.active.as_ref().unwrap().to_string(),
             recovery: moves_info.cargoquery[x].title.recovery.as_ref().unwrap().to_string(),
-            hit: moves_info.cargoquery[x].title.hit.as_ref().unwrap().to_string(),
-            block: moves_info.cargoquery[x].title.block.as_ref().unwrap().to_string(),
-            level: moves_info.cargoquery[x].title.level.as_ref().unwrap().to_string(),
-            riscgain: moves_info.cargoquery[x].title.riscgain.as_ref().unwrap().to_string(),
-            scaling: moves_info.cargoquery[x].title.prorate.as_ref().unwrap().to_string(),
-            counter: moves_info.cargoquery[x].title.counter.as_ref().unwrap().to_string(),
+            on_hit: moves_info.cargoquery[x].title.on_hit.as_ref().unwrap().to_string(),
+            on_block:moves_info.cargoquery[x].title.on_block.as_ref().unwrap().to_string(),
+            level:moves_info.cargoquery[x].title.level.as_ref().unwrap().to_string(),
+            counter:moves_info.cargoquery[x].title.counter.as_ref().unwrap().to_string(),
+            move_type:moves_info.cargoquery[x].title.move_type.as_ref().unwrap().to_string(),
+            risc_gain:moves_info.cargoquery[x].title.risc_gain.as_ref().unwrap().to_string(),
+            risc_loss:moves_info.cargoquery[x].title.risc_loss.as_ref().unwrap().to_string(),
+            wall_damage:moves_info.cargoquery[x].title.wall_damage.as_ref().unwrap().to_string(),
+            input_tension:moves_info.cargoquery[x].title.input_tension.as_ref().unwrap().to_string(),
+            chip_ratio:moves_info.cargoquery[x].title.chip_ratio.as_ref().unwrap().to_string(),
+            otg_ratio: moves_info.cargoquery[x].title.otg_ratio.as_ref().unwrap().to_string(),
+            scaling: moves_info.cargoquery[x].title.scaling.as_ref().unwrap().to_string(),
+            invincibility:moves_info.cargoquery[x].title.invincibility.as_ref().unwrap().to_string(),
+            cancel: moves_info.cargoquery[x].title.cancel.as_ref().unwrap().to_string(),
+            caption:moves_info.cargoquery[x].title.caption.as_ref().unwrap().to_string(),
+            notes:moves_info.cargoquery[x].title.notes.as_ref().unwrap().to_string(),
+            hitbox_caption:moves_info.cargoquery[x].title.hitbox_caption.as_ref().unwrap().to_string(),
         };
 
         vec_processed_moves_info.push(processed_moves_info);
