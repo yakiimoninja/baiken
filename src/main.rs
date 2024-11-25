e0479a1d3073ad821153a3ef4651a397bf66598f
yakiimoninja <github@pns.slmail.me>
2024-11-25 01:41:14 +0000

    remove unnecessary `MoveInfo` fields

--- src/main.rs
+++ src/main.rs
@@ -76,9 +76,9 @@ pub struct MoveInfo {
     cancel: String,
     caption: String,
     notes: String,
-    hitbox_caption: String,
-    //imagesption: String,
-    //hitboxesption: String,
+    //hitbox_caption: String,
+    //images: String,
+    //hitboxes: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
