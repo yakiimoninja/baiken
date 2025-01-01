f71fef2efea3dfd6fc300124d9040462f355b874
yakiimoninja <github@pns.slmail.me>
2025-01-01 20:59:22 +0000

    remove unused imports

--- src/create.rs
+++ src/create.rs
@@ -1,5 +1,5 @@
 use std::{fs, path::Path};
-use rusqlite::{named_params, params, Connection as SqlConnection};
+use rusqlite::{named_params, Connection as SqlConnection};
 use crate::{update::update_all_char_data, Error, MoveAliases, Nicknames, CHARS};
 
 pub async fn create_db() -> Result<(), Error> {
