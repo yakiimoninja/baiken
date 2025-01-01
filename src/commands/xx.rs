7cd01795af531d4c82bb5c7fc9d995b4a0d2e2bc
yakiimoninja <github@pns.slmail.me>
2025-01-01 22:26:40 +0000

    refactor to db instead of json

--- src/commands/xx.rs
+++ src/commands/xx.rs
@@ -1,7 +1,6 @@
-use crate::{check, Context, Error, Gids};
+use crate::{check, Context, Error};
 use colored::Colorize;
-use std::io::Write;
-use std::{fs, fs::OpenOptions};
+use rusqlite::{named_params, Connection as SqlConnection, OpenFlags};
 
 /// Disable or enable easter eggs for current server. Admin only.
 #[poise::command(
@@ -29,70 +28,41 @@ pub async fn xx(
         return Ok(());
     }
 
-    // Reading the gids json
-    let data_from_file =
-        fs::read_to_string("data/gids.json").expect("\nFailed to read 'gids.json' file.");
-
-    // Deserializing from gids json
-    let mut vec_gids = serde_json::from_str::<Gids>(&data_from_file).unwrap();
-
     // Parse user guild id to string
     let guild_id = ctx.guild_id().unwrap().to_string();
+    // Open gids.db
+    let db = SqlConnection::open_with_flags("data/gids.db", OpenFlags::SQLITE_OPEN_READ_WRITE).unwrap();
+    // Check if gid is in db
+    let guild_id_exists = db.prepare("SELECT (gid) FROM gids WHERE gid = :gid").unwrap()
+        .exists(named_params! {":gid": guild_id}).unwrap();
 
     if option == "disable" {
-        // Hand to add guild id to exclusion list
-        for x in vec_gids.id.iter() {
-            // Checking if guild is in the exclusion list
-            if guild_id == *x.to_string() {
-                println!("{}", "Easter eggs are disabled.".purple());
-                ctx.say("Easter eggs for this server are disabled.").await?;
-
-                return Ok(());
-            }
+        // Gid already exists
+        if guild_id_exists {
+            println!("{}", "Easter eggs are already disabled.".purple());
+            ctx.say("Easter eggs for this server are already disabled.").await?;
+            return Ok(());
         }
 
-        // Creating gids json file
-        let mut file = OpenOptions::new()
-            .create(true)
-            .truncate(true)
-            .write(true)
-            .open("data/gids.json")
-            .expect("\nFailed to open 'gids.json' file.");
-
         // Adding guild id to exclusion list
-        vec_gids.id.push(guild_id);
-        let gid_to_json = serde_json::to_vec_pretty(&vec_gids).unwrap();
-        file.write_all(&gid_to_json).unwrap();
-
+        db.execute("INSERT INTO gids (gid) VALUES (:gid)", named_params! {":gid": guild_id}).unwrap();
         println!("{}", "Easter eggs have been disabled.".purple());
         ctx.say("Easter eggs for this server have been disabled.").await?;
     }
     else if option == "enable" {
-        // Hand to remove guild id from exclusion list
-        for x in 0..vec_gids.id.len() {
-            // Checking if guild is in the exclusion list
-            if guild_id == vec_gids.id[x] {
-                // Creating gids json file
-                let mut file = OpenOptions::new()
-                    .create(true)
-                    .write(true)
-                    .truncate(true)
-                    .open("data/gids.json")
-                    .expect("\nFailed to open 'gids.json' file.");
+        // Gid already exists
+        if guild_id_exists {
+            // Removing guild id from exclusion list
+            
+            db.execute("DELETE FROM gids WHERE gid = :gid", named_params! {":gid": guild_id}).unwrap();
 
-                // Removing guild id from exclusion list
-                let _ = vec_gids.id.swap_remove(x);
-                let gid_to_json = serde_json::to_vec_pretty(&vec_gids).unwrap();
-                file.write_all(&gid_to_json).unwrap();
+            println!("{}", "Easter eggs have been enabled".purple());
+            ctx.say("Easter eggs for this server have been enabled.").await?;
 
-                println!("{}", "Easter eggs have been enabled".purple());
-                ctx.say("Easter eggs for this server have been enabled.").await?;
-
-                return Ok(());
-            }
+            return Ok(());
         }
 
-        println!("{}", "Easter eggs are enabled.".purple());
+        println!("{}", "Easter eggs are already enabled.".purple());
         ctx.say("Easter eggs for this server are already enabled.").await?;
         return Ok(());
     }
@@ -100,5 +70,6 @@ pub async fn xx(
         println!("{}", "Mega weird interaction with xx.".red());
         return Ok(());
     }
+
     Ok(())
 }
